0dbb993114d552fe455e93bb8f3a7cccec8ff1e5
kuduzovkhamzat@gmail.com <khamzat995@gmail.com>
2022-05-03 18:15:17 +0300

    03052022

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use("/api", require("./routes"));// для продакшина
-app.use(require("./routes")); // для разработки
+app.use("/api", require("./routes"));// для продакшина
+//app.use(require("./routes")); // для разработки
 
 const start = async () => {
   try {
